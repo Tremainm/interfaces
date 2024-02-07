e69d09fe2be52a454efc3ae73785d1f0c7bf6f08
Tremain <g00367973@atu.ie>
2024-02-07 17:04:53 +0000

    added new dessert class

--- src/main/java/ie/atu/OnlineOrderingApp.java
+++ src/main/java/ie/atu/OnlineOrderingApp.java
@@ -6,12 +6,14 @@ public class OnlineOrderingApp {
         Burger myBurger;
         Pizza myPizza;
         Salad mySalad;
+        Dessert myDessert;
 
         MenuItem[] menuItems = new MenuItem[]
                 {
                     myBurger = new Burger("Classic Burger", 8.99, "Beef patty, lettuce, tomato, cheese"),
                     myPizza = new Pizza("Margherita Pizza", 12.99, "Tomato sauce, mozzarella, basil"),
-                    mySalad = new Salad("Caesar Salad", 6.99, "Romaine lettuce, croutons, Caesar dressing")
+                    mySalad = new Salad("Caesar Salad", 6.99, "Romaine lettuce, croutons, Caesar dressing"),
+                        myDessert = new Dessert("Lemon Cheesecake", 7.99, "Cream cheese, lemon zest, biscuit")
                 };
 
         for(MenuItem menuItem : menuItems)
@@ -23,7 +25,7 @@ public class OnlineOrderingApp {
         double totalCost = 0;
         for(MenuItem menuItem : menuItems)
         {
-            totalCost = myBurger.getPrice() + myPizza.getPrice() + mySalad.getPrice();
+            totalCost += menuItem.getPrice();
         }
         System.out.println("\nTotal cost: €" + totalCost);
     }
